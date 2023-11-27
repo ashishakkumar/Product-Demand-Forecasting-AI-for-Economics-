3f94c3cde260cc3ef812455057fb6869fac37985
Ashish Kumar <86063242+ashishakkumar@users.noreply.github.com>
2023-11-27 22:31:11 +0530

    Update aiforeco.py

--- aiforeco.py
+++ aiforeco.py
@@ -81,7 +81,7 @@ with st.sidebar:
 background_image = """
 <style>
 [data-testid="stAppViewContainer"] > .main {
-    background-image: demand-forecasting.jpg;
+    background-image: url('demand-forecasting.jpg');
     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
     background-position: center;  
     background-repeat: no-repeat;

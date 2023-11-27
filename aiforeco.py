18c96fff2a546b4c52983c56c3ef6deffe36d070
Ashish Kumar <86063242+ashishakkumar@users.noreply.github.com>
2023-11-27 21:50:12 +0530

    Update aiforeco.py

--- aiforeco.py
+++ aiforeco.py
@@ -120,7 +120,7 @@ if choice == 'Home Page' :
     page_bg_img = '''
     <style>
     body {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-image: url("demand-forecasting.jpg");
     background-size: cover;
     }
     </style>
